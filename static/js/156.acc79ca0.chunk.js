e04d8b13e8abbe58151dc71efe8e7acc0fe6bbc7
Isiri-gallage <isirigallage2002@gmail.com>
2025-11-16 01:50:46 +0530

    Updates

--- static/js/156.acc79ca0.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktask_manager=self.webpackChunktask_manager||[]).push([[156],{9156:(e,a,k)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=156.acc79ca0.chunk.js.map
---
+++ static/js/488.44346c1d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktask_manager=self.webpackChunktask_manager||[]).push([[488],{2488:(a,e,s)=>{new Map,new Date}}]);
+//# sourceMappingURL=488.44346c1d.chunk.js.map
